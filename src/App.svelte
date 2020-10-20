4ef9274424a001026ce72788b8ac857110adcb35
Xuriwork <xuriwork@gmail.com>
2020-10-20 14:31:03 -0400

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,98 @@
 <script>
+	import Modal from './Modal.svelte';
+	
+	let editorContent = '';
+	let wordCount = 0;
+	let characterCount = editorContent.length;
+	let lineCount = 0;
+	let fileInput;
+	let filename;
+	
+	const calculateWordCount = () => {
+		const spiltWords = editorContent.trim().split(/\s+/);
+		wordCount = spiltWords.length;
+		
+		characterCount = editorContent.length;
+
+		lineCount = editorContent.split('\n').length;
+		lineCount = editorContent.split(/\r?\n|\r/).length;
+	};
+
+	const fileType = {
+		type: 'text/plain;charset=utf-8'
+	};
+
+	const anchor = document.createElement('a');
+
+	const saveFile = (text, name) => {
+		const blob = new Blob([text], fileType);
+		const objURL = URL.createObjectURL(blob);
+
+		anchor.href = objURL;
+		anchor.download = name;
+		anchor.click();
+		
+		URL.revokeObjectURL(objURL);
+	};
+
+	const openInput = () => fileInput.click();
+	const clearInput = (event) => event.target.value = null;
+	
+	const openFile = (input) => {
+		const file = input.target.files[0];
+		const reader = new FileReader();
+		reader.readAsText(file);
+
+		reader.onload = () => editorContent = reader.result;
+		reader.onerror = () => console.log(reader.error);
+	};
+
 </script>
 
+<div class='app-component'>
+	<div class='editor'>
+		<textarea
+			bind:value={editorContent}
+			placeholder='Enter some text...'
+			id='area'
+			spellcheck='false'
+			class='ember-text-area ember-view' 
+			on:keyup={calculateWordCount}
+		></textarea>
+	</div>
+	<div class='editor-footer'>
+		<div>
+			<span>Words: {wordCount}</span>
+			<span>Characters: {characterCount}</span>
+			<span>Lines: {lineCount}</span>
+		</div>
+		<div class='buttons-container'>
+			<button on:click={openInput}>Open</button>
+			<button on:click={saveFile}>Save</button>
+		</div>
+		<input 
+			type='file' 
+			accept='text/plain' 
+			bind:this={fileInput} 
+			on:change={openFile} 
+			style="display: none;" 
+			on:click={clearInput}
+		/>
+	</div>
+	<Modal filename={filename} />
+</div>
+
 <style type='scss'>
-	.app-component {
+
+	%flex-column-center {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
+	}
+
+	.app-component {
+		@extend %flex-column-center;
 		height: 100%;
 		width: 100%;
 	}
@@ -15,8 +101,11 @@
 		width: 800px;
 		max-width: 95%;
 		height: 500px;
-		border-radius: 5px;
-
+		border-radius: 10px 10px 0 0;
+		background-color: #3A4556;
+		padding: 5px;
+		box-sizing: border-box;
+		
 		textarea {
 			width: 100%;
 			height: 100%;
@@ -24,54 +113,47 @@
 			resize: none;
 			padding: 20px;
 			box-sizing: border-box;
+			background-color: #2D3848;
+			color: #A4AEBA;
+			border: none;
+
+			&::placeholder {
+				color: #A4AEBA;
+			}
 		}
 	}
 
-	
-	form {
-		color: #f8f8f8;
+	.editor-footer {
+		background-color: #3A4556;
+		color: #ffffff;
 		width: 800px;
 		max-width: 95%;
-		margin-top: 10px;
-		
-		.input-group {
-			display: flex;
-			flex-direction: column;
-			width: 100%;
+		border-radius: 0 0 10px 10px;
+		padding-bottom: 8px;
+		padding-left: 30px;
+		padding-right: 30px;
+		padding-top: 2px;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
 
-			input, label {
-				width: 100%;
+		> div {
+
+			span {
+				font-weight: 560;
+
+				&:not(:last-of-type) {
+				margin-right: 10px;
+				}
+			}
+		}
+
+		.buttons-container {
+			button {
+				padding: 0 20px;
 			}
 		}
 	}
 </style>
-
-<div class='app-component'>
-	<div class='editor'>
-		<textarea
-			spellcheck='false'
-			placeholder='Enter some text...'
-			id='area'
-			class='ember-text-area ember-view' />
-		</div>
-		<form>
-
-			<h3>Message Lifetime</h3>
-			<div class='input-group'>
-				<label for='messageLifetime'>Expires after</label>
-				<input type='text' id='messageLifetime'  />
-				
-				<label for='numberOfViews'>or number of views</label>
-				<input type='text' id='numberOfViews'  />
-			</div>
-
-			<h3>Passphrase</h3>
-			<div class='input-group'>
-				<label for='messagePassphrase'>Secure this message with a custom password</label>
-				<input type='text' id='passphrase'  />
-				
-				<label for='confirmMessagePassphrase'>Confirm Passphrase</label>
-				<input type='text' id='confirmMessagePassphrase'  />
-			</div>
-		</form>
-</div>
---
+++ src/Modal.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let filename;
+</script>
+
+<div class='modal-overlay'>
+    <div class='modal'>
+        <label for='filename'>Filename</label>
+        <input type='text' id='filename' value={filename} />
+        <div class='buttons-container'>
+            <button>Cancel</button>
+            <button>Save</button>
+        </div>
+    </div>
+</div>
+
+<style lang='scss'>
+
+    .modal-overlay {
+        display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+        background-color: rgba(0, 0, 0, 0.26);
+        position: fixed;
+        width: 100%;
+        height: 100%;
+
+        .modal {
+            width: 300px;
+            max-width: 95%;
+            height: 130px;
+            background-color: #ffffff;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding: 5px 20px;
+            box-sizing: border-box;
+            border-radius: 5px;
+            
+            .buttons-container {
+                margin-top: 10px;
+                display: flex;
+                justify-content: flex-end;
+
+                button {
+                    padding: 2px 20px;
+
+                    &:first-of-type {
+                        margin-right: 10px;
+                    }
+                }
+            }
+        }
+    }
+</style>
