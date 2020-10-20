0958c991aba9d4625acb9fa3e54f9decde30456a
Xuriwork <xuriwork@gmail.com>
2020-10-19 23:37:36 -0400

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -22,8 +22,26 @@
 			height: 100%;
 			border-radius: 5px;
 			resize: none;
-			padding: 10px;
+			padding: 20px;
 			box-sizing: border-box;
+		}
+	}
+
+	
+	form {
+		color: #ffffff;
+		width: 800px;
+		max-width: 95%;
+		margin-top: 10px;
+		
+		.input-group {
+			display: flex;
+			flex-direction: column;
+			width: 100%;
+
+			input, label {
+				width: 100%;
+			}
 		}
 	}
 </style>
@@ -35,5 +53,25 @@
 			placeholder='Enter some text...'
 			id='area'
 			class='ember-text-area ember-view' />
-	</div>
+		</div>
+		<form>
+
+			<h3>Message Lifetime</h3>
+			<div class='input-group'>
+				<label for='messageLifetime'>Expires after</label>
+				<input type='text' id='messageLifetime'  />
+				
+				<label for='numberOfViews'>or number of views</label>
+				<input type='text' id='numberOfViews'  />
+			</div>
+
+			<h3>Passphrase</h3>
+			<div class='input-group'>
+				<label for='messagePassphrase'>Secure this message with a custom password</label>
+				<input type='text' id='passphrase'  />
+				
+				<label for='confirmMessagePassphrase'>Confirm Passphrase</label>
+				<input type='text' id='confirmMessagePassphrase'  />
+			</div>
+		</form>
 </div>
